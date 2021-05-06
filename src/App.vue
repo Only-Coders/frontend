20a33a77ead35c2cf66bf45c2379bfc3e7a1770e
Mathias Zunino <mathiaszunino@gmail.com>
2021-05-06 18:39:02 -0300

    feat: images

--- src/App.vue
+++ src/App.vue
@@ -10,15 +10,6 @@
           transition="scale-transition"
           width="40"
         />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
       </div>
 
       <v-spacer></v-spacer>

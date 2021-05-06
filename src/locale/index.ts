f7e63c4a690a3aa37b3683ee1fec0dd8e13c92ca
Mathias Zunino <mathiaszunino@gmail.com>
2021-05-06 18:34:45 -0300

    feat: i18n implemented

---
+++ src/locale/index.ts
@@ -0,0 +1,7 @@
+import en from "./en.json";
+import sp from "./sp.json";
+
+export default {
+  sp,
+  en
+};
--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,22 @@ import App from "./App.vue";
 import router from "./router";
 import store from "@/store/index";
 import vuetify from "./plugins/vuetify";
+import VueI18n from "vue-i18n";
+import Locale from "@/locale/index";
+
+Vue.use(VueI18n);
 
 Vue.config.productionTip = false;
+
+const i18n = new VueI18n({
+  locale: store.state.lang,
+  messages: { sp: Locale.sp, en: Locale.en }
+});
 
 new Vue({
   router,
   store,
+  i18n,
   vuetify,
   render: (h) => h(App)
 }).$mount("#app");

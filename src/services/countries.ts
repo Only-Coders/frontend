c1eb259f1747dc11a1c2d8ed4b8324b6f99e5c51
mateosayas <mateosayaspereira@gmail.com>
2021-05-14 01:31:24 -0300

    feat: Add - Get countries service

---
+++ src/services/countries.ts
@@ -0,0 +1,8 @@
+import axios from "@/plugins/axios";
+import store from "@/store";
+import { Country } from "@/models/country";
+
+export async function getCountries(): Promise<Country[]> {
+  const { data } = await axios.get<Country[]>("/api/countries");
+  return data;
+}

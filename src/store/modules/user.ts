e781b7b423dfe65790f1746384d22dc96b843a26
mateosayas <mateosayaspereira@gmail.com>
2021-05-20 23:36:21 -0300

    feat: user module and store

--- src/store/modules/user.ts
+++ src/store/modules/user.ts
@@ -1,15 +1,25 @@
-import { Module, VuexModule, config, getModule } from "vuex-module-decorators";
-import store from "@/store";
+import { Module, VuexModule, Mutation } from "vuex-module-decorators";
 
-config.rawError = true;
-@Module({
-  name: "user",
-  namespaced: true,
-  dynamic: true,
-  store
-})
-class User extends VuexModule {
-  username = "pepe";
+export type UserData = {
+  imageURI: string;
+  firstName: string;
+  lastName: string;
+  canonicalName: string;
+  role: string;
+  completed: string;
+};
+
+@Module({ name: "userModule", namespaced: true })
+export default class User extends VuexModule {
+  user: UserData | null = null;
+
+  @Mutation
+  SET_USER(value: UserData) {
+    this.user = value;
+  }
+
+  @Mutation
+  CLEAR_USER() {
+    this.user = null;
+  }
 }
-
-export default getModule(User);

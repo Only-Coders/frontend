50a8028244cd6ee4d5dad13a02418c589ccb4f44
Mateo Sayas <64172144+mateosayas@users.noreply.github.com>
2021-05-22 00:36:13 -0300

    Update src/services/follow.ts
    Co-authored-by: Mathias Zunino <mathiaszunino@gmail.com>

--- src/services/follow.ts
+++ src/services/follow.ts
@@ -1,11 +1,9 @@
 import axios from "@/plugins/axios";
 
 export async function post_follow(canonicalName: string): Promise<void> {
-  const response = await axios.post(`/api/users/following/${canonicalName}`);
-  return response.data;
+  await axios.post(`/api/users/following/${canonicalName}`);
 }
 
 export async function delete_follow(canonicalName: string): Promise<void> {
-  const response = await axios.delete(`/api/users/following/${canonicalName}`);
-  return response.data;
+  await axios.delete(`/api/users/following/${canonicalName}`);
 }

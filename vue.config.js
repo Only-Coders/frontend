499c50f51b98beb65b395cd313ac5120c5bc53cf
Mariano Z <marianoz@zoho.com>
2021-05-29 13:42:15 -0300

    build: add site title

--- vue.config.js
+++ vue.config.js
@@ -6,5 +6,11 @@ module.exports = {
         target: `https://api.onlycoders.tech`
       }
     }
+  },
+  chainWebpack: (config) => {
+    config.plugin("html").tap((args) => {
+      args[0].title = "OnlyCoders";
+      return args;
+    });
   }
 };

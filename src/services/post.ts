9956528b7a7923d11aa786b9c56a9c18deae5c4a
mateosayas <mateosayaspereira@gmail.com>
2021-05-29 18:29:24 -0300

    feat: added service, model and started view post component

--- src/services/post.ts
+++ src/services/post.ts
@@ -1,6 +1,13 @@
 import axios from "@/plugins/axios";
 import { Post } from "@/models/post";
+import { Pagination } from "@/models/Pagination/pagination";
+import { GetPost } from "@/models/post";
 
 export async function post(post: Post): Promise<void> {
   await axios.post(`/api/posts`, post);
+}
+
+export async function getPost(): Promise<Pagination<GetPost>> {
+  const response = await axios.get(`/api/feed-posts`);
+  return response.data;
 }

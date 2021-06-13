52e87de7a708fe96e846460058d7afbde01e340d
Mathias Zunino <mathiaszunino@gmail.com>
2021-06-13 14:00:13 -0300

    feat(dash): users quantity report

---
+++ src/models/usersQuantity.ts
@@ -0,0 +1,5 @@
+export type UsersQuantity = {
+  totalActiveUsers: number;
+  totalBlockedUsers: number;
+  totalBannedUsers: number;
+};
--- src/views/AdminDash.vue
+++ src/views/AdminDash.vue
@@ -1,32 +1,32 @@
 <template>
   <div>
     <v-row no-gutters class="my-12" justify="center">
-      <v-col cols="3">
-        <v-card :style="'border-top: 4px solid #3F51B5'">
-          <v-card-title>Bloqueos en el ultimo mes</v-card-title>
-          <v-card-text class="d-flex">
-            <v-icon size="52" color="#3F51B5">mdi-account-cancel</v-icon>
-            <p class="text-h3 mb-0 pl-5">420</p>
-          </v-card-text>
-        </v-card>
+      <v-col cols="10" sm="5" md="3" class="my-2 mx-sm-2 mx-md-5 mx-lg-10 mx-xl-16">
+        <dash-card
+          :color="'#2E7D32'"
+          icon="mdi-earth"
+          :message="$i18n.t('Admin.totalActiveUsers')"
+          :quantity="usersQuantity.totalActiveUsers"
+          :isLoading="isLoading"
+        ></dash-card>
       </v-col>
-      <v-col cols="3 offset-1">
-        <v-card :style="'border-top: 4px solid #009688'">
-          <v-card-title>Bloqueos en el ultimo mes</v-card-title>
-          <v-card-text class="d-flex">
-            <v-icon size="52" color="#009688">mdi-account-cancel</v-icon>
-            <p class="text-h3 mb-0 pl-5">420</p>
-          </v-card-text>
-        </v-card>
+      <v-col cols="10" sm="5" md="3" class="my-2 mx-sm-2 mx-md-5 mx-lg-10 mx-xl-16">
+        <dash-card
+          :color="'#3F51B5'"
+          icon="mdi-account-cancel"
+          :message="$i18n.t('Admin.totalBlockedUsers')"
+          :quantity="usersQuantity.totalBlockedUsers"
+          :isLoading="isLoading"
+        ></dash-card>
       </v-col>
-      <v-col cols="3 offset-1">
-        <v-card :style="'border-top: 4px solid #5E35B1'">
-          <v-card-title>Bloqueos en el ultimo mes</v-card-title>
-          <v-card-text class="d-flex">
-            <v-icon size="52" color="#5E35B1">mdi-account-cancel</v-icon>
-            <p class="text-h3 mb-0 pl-5">420</p>
-          </v-card-text>
-        </v-card>
+      <v-col cols="10" sm="5" md="3" class="my-2 mx-sm-2 mx-md-5 mx-lg-10 mx-xl-16">
+        <dash-card
+          :color="'var(--v-secondary-base)'"
+          icon="mdi-ghost-off-outline"
+          :message="$i18n.t('Admin.totalBannedUsers')"
+          :quantity="usersQuantity.totalBannedUsers"
+          :isLoading="isLoading"
+        ></dash-card>
       </v-col>
     </v-row>
     <v-row no-gutters justify="space-around">
@@ -78,11 +78,19 @@
 
 <script lang="ts">
 import Vue from "vue";
+import DashCard from "@/components/Admin/DashCard.vue";
+import { getUsersQuantity } from "@/services/data-report";
+import { UsersQuantity } from "@/models/usersQuantity";
 
 export default Vue.extend({
   name: "AdminDash",
+
+  components: { DashCard },
+
   data() {
     return {
+      usersQuantity: {} as UsersQuantity,
+      isLoading: true,
       series3: [
         {
           name: "series1",
@@ -419,6 +427,11 @@ export default Vue.extend({
         }
       }
     };
+  },
+
+  async created() {
+    this.usersQuantity = await getUsersQuantity();
+    this.isLoading = false;
   }
 });
 </script>

95be05db677ea52fe9ceedfa246c424e239bd1e9
mateoKaizen1998 <mateosayas@kaizensoftworks.com>
2021-06-28 17:32:36 -0300

    fix: drogas y mas drogas. No las usen y programen

--- src/store/modules/userPagination.ts
+++ src/store/modules/userPagination.ts
@@ -3,8 +3,8 @@ import { User } from "@/models/user";
 import { Pagination } from "@/models/Pagination/pagination";
 
 @Module({ name: "userPaginationModule", namespaced: true })
-export default class adminPagination extends VuexModule {
-  adminPagination: Pagination<User> | null = null;
+export default class UserPagination extends VuexModule {
+  userPagination: Pagination<User> | null = null;
   search = "";
   usersPerPage = 1;
   country = "";
@@ -13,12 +13,12 @@ export default class adminPagination extends VuexModule {
 
   @Mutation
   SET_USER_PAGINATION(value: Pagination<User>): void {
-    this.adminPagination = value;
+    this.userPagination = value;
   }
 
   @Mutation
   CLEAR_USER_PAGINATION(): void {
-    this.adminPagination = null;
+    this.userPagination = null;
   }
 
   @Mutation
